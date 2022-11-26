8404a73ee0685c6a30da1d755f8b5f6f759bb156
Mohd Faraz <55929239+fzmalik@users.noreply.github.com>
2022-11-26 22:54:19 +0000

    first query

---
+++ -- SQLite.sql
@@ -0,0 +1,6 @@
+-- SQLite
+-- SELECT FirstName,LastName,Email from Customers Order by LastName ASC;
+-- CREATE TABLE Anniversaryattendees( CustomerID integer,
+-- PartySize integer
+-- );
+SELECT * from Dishes ORDER by Type;
